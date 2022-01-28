8f99b8be6ca49c9909c1f46ae21c06393d6ae4e8
Fran <fjnf@outlook.com>
2022-01-28 19:02:48 -0300

    app stopwatch 2

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Menu()
+{
+    while (true)
+    {
+        System.Console.Clear();
+        System.Console.WriteLine("Quanto tempo deseja contar?");
+        System.Console.WriteLine("m -- minutos");
+        System.Console.WriteLine("s -- segundos");
+        System.Console.WriteLine("0 -- sair");
+
+        var tempo = System.Console.ReadLine();
+        Int64 segundos = 0;
+
+        if (tempo != null && tempo[tempo.Length - 1] == 'm')
+        {
+            segundos = Convert.ToInt64(tempo.Substring(0, tempo.Length - 1)) * 60;
+        }
+        else if (tempo != null && tempo[tempo.Length - 1] == 's')
+        {
+            segundos = Convert.ToInt64(tempo.Substring(0, tempo.Length - 1));
+        }
+        else if (tempo == "0")
+        {
+            Console.Clear();
+            System.Environment.Exit(0);
+        }
+        for (int i = 1; i <= segundos; i++)
+        {
+            System.Console.Write("{0} ", i);
+            Thread.Sleep(300);
+        }
+    }
+}
+
+Menu();
