1162f9df641a9c25462d818af111764fbe5e2224
Fran <fjnf@outlook.com>
2022-02-09 10:48:14 -0300

    app obj ori fund 8

--- Content/Balta/ContentContext/Course.cs
+++ Content/Balta/ContentContext/Course.cs
@@ -1,6 +1,21 @@
+using System.Collections.Generic;
+
 namespace Balta.ContentContext
 {
     public class Course : Content
+    {
+        public Course()
+        {
+
+        }
+
+        public string Tag { get; set; }
+
+        public IList<Module> Modules { get; set; }
+
+    }
+
+    public class Module
     {
 
     }
