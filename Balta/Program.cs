6ecb88f5d58a33f9e46ae600858c4b2e58ee208d
Fran <fjnf@outlook.com>
2022-02-09 01:46:18 -0300

    app obj ori fund 1

---
+++ Balta/Program.cs
@@ -0,0 +1,12 @@
+﻿using Balta.ContentContext;
+
+namespace Balta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var course = new Course();
+        }
+    }
+}
